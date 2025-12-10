21d61fcd9e701b9e7656237625e532c35610af53
Sakshi Deshmukh <sakshideshmukh8208412388@gmail.com>
2025-12-10 20:04:53 +0530

    Grometry Commit

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 print("Sakshi")
 import arithmethic
+import geometry
 
-num1=int(input("Enter number 1"))
-num2=int(input("Enter number 2"))
+num1=int(input("Enter number 1:"))
+num2=int(input("Enter number 2:"))
 
 arithmethic.sum(num1,num2)
 arithmethic.sub(num1,num2)
+
+geometry.area(num1,num2)
+geometry.perimeter(num1,num2)
+
+
 
