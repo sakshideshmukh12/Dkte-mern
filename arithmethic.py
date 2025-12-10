5867caa22f36fdc050860025d9ffecbda935f5ab
Sakshi Deshmukh <sakshideshmukh8208412388@gmail.com>
2025-12-10 19:52:29 +0530

    Arithmetic commit

---
+++ arithmethic.py
@@ -0,0 +1,5 @@
+def sum(num1,num2):
+    print(num1+num2)
+
+def sub(num1,num2):
+    print(num1-num2)
--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Sakshi")
+import arithmethic
+
+num1=int(input("Enter number 1"))
+num2=int(input("Enter number 2"))
+
+arithmethic.sum(num1,num2)
+arithmethic.sub(num1,num2)
+
